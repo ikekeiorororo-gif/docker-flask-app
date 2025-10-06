fb7cf5827dcb9b4b4dfb10aa1b928c57a2ef2974
ikekeiorororo-gif <ikekeiorororo@gmail.com>
2025-10-06 21:55:41 +0900

    Add /about route

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask in Docker!"
+    return "Hello from suz Flask in Docker!"
+
+@app.route('/about')
+def about():
+    return "This is the About page of my suz Flask app."
 
 if __name__ == "__main__":
     # host="0.0.0.0" でコンテナ外からアクセス可能にする
