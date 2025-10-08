47587a3326b63b78457b60cd888d0058c2637b2b
ikekeiorororo <suzukij@ryomo.co.jp>
2025-10-08 10:35:06 +0900

    test check api

--- app.py
+++ app.py
@@ -7,10 +7,24 @@ load_dotenv()
 
 app = Flask(__name__)
 
+#てすと見るだけ
+API_URL = "https://api-inference.huggingface.co/models/distilgpt2"
+API_KEY = os.environ.get("HUGGINGFACE_API_KEY")
+headers = {"Authorization": f"Bearer {API_KEY}"}
+
+response = requests.post(API_URL, headers=headers, json={"inputs": "Hello world"})
+print(response.status_code)
+print(response.json())
+
+
 # 無料で使えるモデル URL に変更
 HUGGINGFACE_API_URL = "https://api-inference.huggingface.co/models/distilgpt2"
 HUGGINGFACE_API_KEY = os.environ.get("HUGGINGFACE_API_KEY")
 headers = {"Authorization": f"Bearer {HUGGINGFACE_API_KEY}"}
+
+
+
+
 
 @app.route('/')
 def hello():
